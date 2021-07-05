29804da5763cb07b52b9f6b2c5bb7a2e5d3e71b9
ldh3907 <ldh165163@gmail.com>
2021-07-05 12:20:16 +0900

    add : 행렬과 배열
    add : 행렬과 배열

---
+++ r_lec/step01_basic/d_논리형벡터.r
@@ -0,0 +1,17 @@
+#logical vector : TRUE, FALSE로 이루어진 데이터
+# 따옴표 없이 대문자로 사용
+
+v4 <- c(TRUE, FALSE, TRUE, FALSE)
+v4
+
+class(v4)
+str(v4)
+
+v5 <- c(1,2,3,4,5)
+v5 > 3
+
+v6 <- c(1,1,0,0)
+v7<- c(0,1,1,0)
+v6 & v7
+v6|v7
+
