b7f914da2107ba5ca136aa5ff8b2fbcb0a28bf22
ldh3907 <ldh165163@gmail.com>
2021-07-06 10:54:30 +0900

    update : b_산술연산자
    update : b_산술연산자

---
+++ r_lec/step02_operator/b_논리연산자.r
@@ -0,0 +1,54 @@
+#논리연산자
+x <- 10
+y <- 30
+
+#less than
+x<y
+
+#less than or equal to
+x <= y
+
+#greater than
+x > y
+
+#greater than or equal to
+x >= y
+
+#exacltly equal to
+x ==y
+ 
+#not equal to 
+x != y
+
+# not x
+!x
+
+# x or y , element wise
+x|y
+
+# x OR y,first element of vector 
+x <- c(0,1,0)
+y <- 1
+x||y
+
+# x And y , element wise
+x&y
+
+# x And y,first element of vector 
+x&&y
+
+xor(x,y)
+
+#============================================================
+a <- c(10,20,30,40,50)
+b <- 30
+
+a<b
+
+a <= b
+a > b
+a != b
+
+a+b
+a/b
+
