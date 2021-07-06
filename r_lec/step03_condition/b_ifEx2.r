9f3c894b8cd2448361253e103903b2e03821892a
ldh3907 <ldh165163@gmail.com>
2021-07-06 11:44:11 +0900

    update : b_ifEx2
    update : b_ifEx2

---
+++ r_lec/step03_condition/b_ifEx2.r
@@ -0,0 +1,52 @@
+#if~else
+# ¹®¹ý if(cond) expr1 else expr2
+
+v1 <- 10
+
+if(v1>50){
+  print(V1)
+}else{
+  print(v1*10)
+  print(v1/10) 
+}
+
+#------------------------------------------------------
+
+v2 <- 10
+v3 <- 20
+
+if(v2>v3){
+  v4 <- v2
+}else{v5 <- v3 
+}
+v5
+v4
+
+#-----------------------------------------------------
+
+p1
+
+if(pi < 3){
+  printf("expr if statement")
+}else{
+  printf("expr else statement")
+}
+
+#-----------------------------------------------------
+#ÁßÃ¸ if¹® 
+v6 <- c(-5,-4,-3,-2,-1,0,1,2,3,4,5)
+length(v6)
+
+if(length(V6) == 11){
+    if(sum(v6)==55){
+      print("expr if statement")
+    }else{
+      print(sum(v6))
+    }
+  }else{
+    print("vector v6 length != 11")
+}
+
+
+
+
